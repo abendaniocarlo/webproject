7d1c960d52f751635d1f31b476a556f8302b89c3
CARLO <carlocabendanio@gmail.com>
2017-09-09 13:30:35 +0800

    initial file

---
+++ world.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World App</title>
+</head>
+<body>
+	<?php
+		for($ctr=1;$ctr<=6;$ctr++){
+			echo "<h$ctr> Hello ser! How's ur day?</h$ctr>";
+		}
+	?>
+</body>
+</html>
